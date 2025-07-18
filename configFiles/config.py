5318cc1a52c723cf4b24d2fd5db4d5adae722e68
Ganpat Patel <ganpat.patel.012@gmail.com>
2025-07-18 22:13:27 +0200

    Setting Up Docker Setup for the webapp

---
+++ configFiles/config.py
@@ -0,0 +1,12 @@
+API_URL = "http://dsa8_fastapi:8000"
+
+DB_CONFIG = {
+    "dbname": "dsa8_db",
+    "user": "dsa8_user",
+    "password": "dsa8_pass",
+    "host": "dsa8_postgres",
+    "port": "5432"
+}
+
+# Teams webhook URL
+TEAMS_WEBHOOK_URL = "https://epitafr.webhook.office.com/webhookb2/463ace8d-55de-416c-af6f-0d944ddcd0c6@3534b3d7-316c-4bc9-9ede-605c860f49d2/IncomingWebhook/a7dbae33a66749eeb3efcc25efc803c5/2ee1ac5b-a7da-4763-a97e-b0cee3001618/V2ZZMMvvk6BDS_C8lVdGEjl2sC4o53k_pAyPRC0xlj6iE1"
